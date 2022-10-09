f638646322686ee5838354878c35f99ceffa4443
Anton Kurilov <anton0kurilov@gmail.com>
2022-10-09 13:58:50 +0700

    refactor: use prettier for js

--- src/scripts/_admin.js
+++ src/scripts/_admin.js
@@ -1,55 +1,55 @@
-const darkModeStatus = localStorage.getItem('darkMode');
-const paletteStatus = localStorage.getItem('palette');
-const body = document.body;
+const darkModeStatus = localStorage.getItem('darkMode')
+const paletteStatus = localStorage.getItem('palette')
+const body = document.body
 if (darkModeStatus == 'true') {
-    body.classList.add('dark');
+    body.classList.add('dark')
 }
 if (paletteStatus) {
-    body.classList.add(paletteStatus);
+    body.classList.add(paletteStatus)
 }
 
-const switchMode = document.querySelector('#switchMode');
-switchMode.addEventListener('click', function() {
+const switchMode = document.querySelector('#switchMode')
+switchMode.addEventListener('click', function () {
     if (body.classList.contains('dark') === false) {
-        body.classList.add('dark');
-        localStorage.setItem('darkMode', true);
+        body.classList.add('dark')
+        localStorage.setItem('darkMode', true)
     } else {
-        body.classList.remove('dark');
-        localStorage.removeItem('darkMode');
+        body.classList.remove('dark')
+        localStorage.removeItem('darkMode')
     }
 })
 
-const showPalette = document.querySelector('#showPalette');
-showPalette.addEventListener('click', function() {
-    const paletteBody = document.getElementsByClassName('admin__palette');
+const showPalette = document.querySelector('#showPalette')
+showPalette.addEventListener('click', function () {
+    const paletteBody = document.getElementsByClassName('admin__palette')
     if (paletteBody[0].style.display == 'flex') {
-        paletteBody[0].style.display = 'none';
+        paletteBody[0].style.display = 'none'
     } else {
-        paletteBody[0].style.display = 'flex';
+        paletteBody[0].style.display = 'flex'
     }
 })
 
-const yellowPalette = document.querySelector('.admin__palette-yellow');
-const redPalette = document.querySelector('.admin__palette-red');
-const greenPalette = document.querySelector('.admin__palette-green');
+const yellowPalette = document.querySelector('.admin__palette-yellow')
+const redPalette = document.querySelector('.admin__palette-red')
+const greenPalette = document.querySelector('.admin__palette-green')
 
-yellowPalette.addEventListener('click', function() {
-    body.classList.add('yellow');
-    body.classList.remove('red');
-    body.classList.remove('green');
-    localStorage.setItem('palette', 'yellow');
+yellowPalette.addEventListener('click', function () {
+    body.classList.add('yellow')
+    body.classList.remove('red')
+    body.classList.remove('green')
+    localStorage.setItem('palette', 'yellow')
 })
 
-redPalette.addEventListener('click', function() {
-    body.classList.add('red');
-    body.classList.remove('yellow');
-    body.classList.remove('green');
-    localStorage.setItem('palette', 'red');
+redPalette.addEventListener('click', function () {
+    body.classList.add('red')
+    body.classList.remove('yellow')
+    body.classList.remove('green')
+    localStorage.setItem('palette', 'red')
 })
 
-greenPalette.addEventListener('click', function() {
-    body.classList.add('green');
-    body.classList.remove('red');
-    body.classList.remove('yellow');
-    localStorage.setItem('palette', 'green');
+greenPalette.addEventListener('click', function () {
+    body.classList.add('green')
+    body.classList.remove('red')
+    body.classList.remove('yellow')
+    localStorage.setItem('palette', 'green')
 })
--- src/scripts/_aside.js
+++ src/scripts/_aside.js
@@ -1,14 +1,14 @@
-const asideElement = document.querySelector('.aside');
-const overlayElement = document.querySelector('.overlay');
-const showAside = document.querySelector('#showAside');
-const hideAside = document.querySelector('#hideAside');
+const asideElement = document.querySelector('.aside')
+const overlayElement = document.querySelector('.overlay')
+const showAside = document.querySelector('#showAside')
+const hideAside = document.querySelector('#hideAside')
 
-showAside.addEventListener('click', function(){
-    asideElement.style.right = '0px';
-    overlayElement.style.display = 'block';
+showAside.addEventListener('click', function () {
+    asideElement.style.right = '0px'
+    overlayElement.style.display = 'block'
 })
 
-hideAside.addEventListener('click', function() {
-    asideElement.style.right = '-320px';
-    overlayElement.style.display = 'none';
+hideAside.addEventListener('click', function () {
+    asideElement.style.right = '-320px'
+    overlayElement.style.display = 'none'
 })
